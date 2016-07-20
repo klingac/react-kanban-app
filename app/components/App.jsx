0dfad9e7a2c74c2e655e3481a5881155d1b4f228
klingac <devklingac@gmail.com>
2016-07-20 17:06:18 +0200

    prepisanie komponenty App do class based

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import Notes from './Notes';
-
 import uuid from 'uuid';
 
-const notes = [
-	{
-		id: uuid.v4(), 
-		task: 'Learn React'
-	},
-	{
-		id:  uuid.v4(),
-		task: 'Do laundry'
-	}
-];
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
 
-export default () => (
-	<div>
-		<button onClick={() => console.log('add note')}>+</button>
-		<Notes notes={notes}/>
-	</div>
-);
+		this.state = {
+			notes: [
+				{
+					id: uuid.v4(), 
+					task: 'Learn React'
+				},
+				{
+					id:  uuid.v4(),
+					task: 'Do laundry'
+				}
+			]
+		};
+	}
+	render() {
+		const {notes} = this.state;
+
+		return (
+			<div>
+				<button onClick={() => console.log('add note')}>+</button>
+				<Notes notes={notes}/>
+			</div>
+		);
+	}
+}
